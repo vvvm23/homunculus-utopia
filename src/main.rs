02ac86a8fe9466718aebcbc498e00b6a353e7c51
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-11-24 16:33:38 +0000

    Implements "Ugg" command Ugg

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,13 @@ const FACE_EMOJI: &str = "<:Face:780055050923278346>";
 const LEGS_EMOJI: &str = "<:Legs:780055050663362570>";
 
 const HELP_COMMAND: &str = "!help";
+const UGG_COMMAND: &str = "!ugg";
 
 struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
+        println!("-> {}", msg.content);
         if msg.content == HELP_COMMAND {
             let help_message = format!("
             > Welcome to Homunculus Utopia.\n\
@@ -37,6 +39,11 @@ impl EventHandler for Handler {
             pad=":black_small_square:", hat=FEDORA_EMOJI, larm=LARM_EMOJI, rarm=RARM_EMOJI, face=FACE_EMOJI, legs=LEGS_EMOJI);
 
             if let Err(e) = msg.channel_id.say(&ctx.http, help_message).await {
+                println!("! Error sending message: {:?}", e);
+            }
+        } else if msg.content == UGG_COMMAND {
+            let ugg_message = format!("\"Ugg\" {emoji}", emoji=":gorilla:");
+            if let Err(e) = msg.channel_id.say(&ctx.http, ugg_message).await {
                 println!("! Error sending message: {:?}", e);
             }
         }
